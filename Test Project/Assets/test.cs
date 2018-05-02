b42c936cd053696eda3d9f177cbb63e38ead89fd
overdose-0504 <handa.0504.azbb@gmail.com>
2018-05-02 17:47:46 +0900

    manzi

---
+++ Test Project/Assets/test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int i;
+        for (i = 3; i >= 0; i--)
+        {
+            Debug.Log(i);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
